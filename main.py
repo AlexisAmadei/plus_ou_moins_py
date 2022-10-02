a1ed5ca8c7f94f302f7a9e6c4b67c8ece2ede577
Alexis Amadei <alexisamadei@gmail.com>
2022-10-03 01:06:34 +0200

    Edit play again
    ability to play again after the victory

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import random
 
-inp = input("Enter the max number: ")
-random_number = random.randint(1, int(inp))
-found = False
-nb_tries = 0
+play_again = True
 
-print("Random max range is set to: " + inp)
-print("----------")
-while found == False:
-    guess = input("What is your guess ?: ")
-    guess = int(guess)
-    if guess == random_number:
-        print("You guessed it in " + str(nb_tries) + " times !")
-        found = True
-    if guess > random_number:
-        print("Your guess is too high !")
-        nb_tries += 1
-    if guess < random_number:
-        print ("Your guess is too low")
-        nb_tries += 1
-    print('----------')
-print("Game over. Play again ?")
+while True:
+    if play_again == True:
+        found = False
+        nb_tries = 1
+        inp = input("Enter the max number: ")
+        random_number = random.randint(1, int(inp))
+        print("Random max range is set to: " + inp)
+        print("----------")
+    while found == False:
+        guess = input("What is your guess ?: ")
+        guess = int(guess)
+        if guess == random_number:
+            print("You guessed it in " + str(nb_tries) + " times !")
+            found = True
+        if guess > random_number:
+            print("Your guess is too high !")
+            nb_tries += 1
+        if guess < random_number:
+            print ("Your guess is too low")
+            nb_tries += 1
+        print('----------')
+    # end while
+    inp = input("Play again ? Type 'yes' or 'no'\n")
+    if inp == "yes":
+        found = False
+        play_again = True
+        print("\n")
+    elif inp == "no":
+        break
+    else:
+        play_again = False
 # end while
